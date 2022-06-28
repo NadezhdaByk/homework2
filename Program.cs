31938e67b4632d5d6a5c2d021f96c2f44b873dfa
Nadezhda <nbykova79@gmail.com>
2022-06-28 22:04:32 +0300

    сохранение

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// найти максимальное из 3-х чисел
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 >= num2 && num1 >= num3) 
+
+{
+    Console.Write("максимальное число ");
+    Console.WriteLine(num1);
+}
+if (num2 > num1 && num2 >= num3) 
+{
+    Console.Write("максимальное число ");
+    Console.WriteLine(num2);
+}
+if (num3 > num1 && num3 > num2) 
+{
+    Console.Write("максимальное число ");
+    Console.WriteLine(num3);
+}
